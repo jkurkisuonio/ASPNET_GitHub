9f85127c6b3df3a87161bd541af875d2f756fb7e
jkurkisuonio <jkurkisuonio@gmail.com>
2015-03-22 19:14:13 +0200

    Testataan GITHub VisualStudion kanssa
    Testataan

---
+++ GITTest/GITTest/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GITTest.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ GITTest/GITTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GITTest.WebApiApplication" Language="C#" %>
